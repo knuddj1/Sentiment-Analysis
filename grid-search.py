8b7d424bbd51e6a38234ea7a7c91534c9e5cc1c8
Dean Knudson (1000025498) <KNUDDJ1@student.op.ac.nz>
2018-08-16 16:26:10 +1200

    Changing result data saving

--- grid-search.py
+++ grid-search.py
@@ -16,93 +16,98 @@ from keras.callbacks import EarlyStopping
 from sklearn.utils import shuffle
 from gensim.parsing.preprocessing import *
 
-CUSTOM_FILTERS = [  lambda x: x.lower(), #To lowercase
-                    lambda text: re.sub(r'https?:\/\/.*\s', '', text, flags=re.MULTILINE), #To Strip away URLs
-                    strip_tags, #Remove tags from s using RE_TAGS.
-                    strip_non_alphanum,#Remove non-alphabetic characters from s using RE_NONALPHA.
-                    strip_punctuation, #Replace punctuation characters with spaces in s using RE_PUNCT.
-                    strip_numeric, #Remove digits from s using RE_NUMERIC.
-                    strip_multiple_whitespaces,#Remove repeating whitespace characters (spaces, tabs, line breaks) from s and turns tabs & line breaks into spaces using RE_WHITESPACE.
-                    remove_stopwords, # Set of 339 stopwords from Stone, Denis, Kwantes (2010).
-                    lambda x: strip_short(x, minsize=3), #Remove words with length lesser than minsize from s.
-                ]
+# CUSTOM_FILTERS = [  lambda x: x.lower(), #To lowercase
+#                     lambda text: re.sub(r'https?:\/\/.*\s', '', text, flags=re.MULTILINE), #To Strip away URLs
+#                     strip_tags, #Remove tags from s using RE_TAGS.
+#                     strip_non_alphanum,#Remove non-alphabetic characters from s using RE_NONALPHA.
+#                     strip_punctuation, #Replace punctuation characters with spaces in s using RE_PUNCT.
+#                     strip_numeric, #Remove digits from s using RE_NUMERIC.
+#                     strip_multiple_whitespaces,#Remove repeating whitespace characters (spaces, tabs, line breaks) from s and turns tabs & line breaks into spaces using RE_WHITESPACE.
+#                     remove_stopwords, # Set of 339 stopwords from Stone, Denis, Kwantes (2010).
+#                     lambda x: strip_short(x, minsize=3), #Remove words with length lesser than minsize from s.
+#                 ]
 
 
 
-def prepare_labels(arr, n_cats):
-    n_samples = len(arr)
-    y = np.zeros(shape=(n_samples, n_cats))
-    for i in range(n_samples):
-        label = arr[i]
-        if label == -1:
-            y[i, 0] = 1.
-        elif label == 0:
-            y[i, 1] = 1.
-        else:
-            y[i, 2] = 1.
-    return y
+# def prepare_labels(arr, n_cats):
+#     n_samples = len(arr)
+#     y = np.zeros(shape=(n_samples, n_cats))
+#     for i in range(n_samples):
+#         label = arr[i]
+#         if label == -1:
+#             y[i, 0] = 1.
+#         elif label == 0:
+#             y[i, 1] = 1.
+#         else:
+#             y[i, 2] = 1.
+#     return y
     
 
 
-labels = []
-data = []
+# labels = []
+# data = []
 
-print('collecting labels')
-with open('labels.csv', 'r') as f:
-    for row in csv.reader(f):
-        labels.append(int(row[0]))
-f.close()
-print('collected labels successfully')
+# print('collecting labels')
+# with open('labels.csv', 'r') as f:
+#     for row in csv.reader(f):
+#         labels.append(int(row[0]))
+# f.close()
+# print('collected labels successfully')
 
-print('collecting data')
-with open('data.csv', 'r') as f:
-    for row in csv.reader(f):
-        nums = [0] * len(row)
-        for i, d in enumerate(row):
-            nums[i] = int(d)
-        data.append(nums)
-f.close()
-print('collected data successfully')
+# print('collecting data')
+# with open('data.csv', 'r') as f:
+#     for row in csv.reader(f):
+#         nums = [0] * len(row)
+#         for i, d in enumerate(row):
+#             nums[i] = int(d)
+#         data.append(nums)
+# f.close()
+# print('collected data successfully')
 
 
 MAX_SIZE = 500
 NUM_CATEGORIES = 3
 
-print('preparing data..')
-x_train = pad_sequences(data, maxlen=max_size, truncating='post', padding='pre')
-y_train = prepare_labels(labels, NUM_CATEGORIES)  
+# print('preparing data..')
+# x_train = pad_sequences(data, maxlen=max_size, truncating='post', padding='pre')
+# y_train = prepare_labels(labels, NUM_CATEGORIES)  
 
-del data
-del labels
+# del data
+# del labels
 
-x_train, y_train = shuffle(x_train, y_train, random_state=0)
+# x_train, y_train = shuffle(x_train, y_train, random_state=0)
 
-with open('word_to_index_top_30000.json', 'r') as f:
-    d = json.load(f)
+# with open('word_to_index_top_30000.json', 'r') as f:
+#     d = json.load(f)
 
-data = []
-labels = []
+# data = []
+# labels = []
 
-with open('test_data.csv', 'r', encoding="utf8") as csvfile:
-    reader = csv.reader(csvfile)
-    for r in reader:
-        words = preprocess_string(r[0], CUSTOM_FILTERS)
-        nums = [0] * len(words)
-        for i, word in enumerate(words):
-            if word in d:
-                nums[i] = d[word]
-        data.append(nums)
-        labels.append(r[-1])
+# with open('test_data.csv', 'r', encoding="utf8") as csvfile:
+#     reader = csv.reader(csvfile)
+#     for r in reader:
+#         words = preprocess_string(r[0], CUSTOM_FILTERS)
+#         nums = [0] * len(words)
+#         for i, word in enumerate(words):
+#             if word in d:
+#                 nums[i] = d[word]
+#         data.append(nums)
+#         labels.append(r[-1])
                 
-x_test = pad_sequences(data[1:], maxlen=59, truncating='post', padding='pre')
-y_test = prepare_labels(labels[1:], NUM_CATEGORIES)
+# x_test = pad_sequences(data[1:], maxlen=59, truncating='post', padding='pre')
+# y_test = prepare_labels(labels[1:], NUM_CATEGORIES)
 
-del data
-del labels
-del nums
-del d
-del words
-del reader
+# del data
+# del labels
+# del nums
+# del d
+# del words
+# del reader
+
+x_train = np.zeros(shape=(10, 500))
+x_test = np.zeros(shape=(10, 500))
+y_train = np.zeros(shape=(10, 3))
+y_test = np.zeros(shape=(10, 3))
 
 print('data prepared.')     
 
@@ -119,6 +124,8 @@ base_dir = 'models {} epochs/'.format(NB_EPOCHS)
 
 if not os.path.exists(base_dir):
     os.mkdir(base_dir)
+
+results = []
 
 for emb_size in EMBEDDING_SIZES:
     for recur_layer in RECURRENT_LAYERS_TYPES:
@@ -152,13 +159,18 @@ for emb_size in EMBEDDING_SIZES:
 
             os.mkdir(model_dir)
             os.chdir(model_dir)
-            headers = ['acc', 'loss', 'val_acc', 'val_loss', 'test_acc', 'test_loss']
-            results = [acc[-1], loss[-1], val_acc[-1], val_loss[-1], test_acc, test_loss]
-            with open('results.csv', 'w') as f:
-                w = csv.writer(f)
-                w.writerow(headers)
-                w.writerow(results)
-
+            results_dic = {
+                'acc': acc,
+                'loss': loss,
+                'val_acc': val_acc,
+                'val_loss': val_loss,
+                'test_acc': test_acc,
+                'test_loss': test_loss
+            }
+            with open('results.json', 'w') as f:
+                json.dump(results_dic, f)
+            
+            results.append({model_info: results_dic})
             model_json = model.to_json()
             with open("model.json", "w") as json_file:
                 json_file.write(model_json)
@@ -169,4 +181,5 @@ for emb_size in EMBEDDING_SIZES:
             os.chdir('..')
 
 
-            
+with open('all_results.json', 'w') as f:
+    json.dump(results, f)            
