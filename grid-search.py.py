0c1c40df9624f10fc583c207f83d536d813666d6
Dean <Dean>
2018-08-16 14:01:45 +1200

    grid search script

--- grid-search.py.py
+++ grid-search.py.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import numpy as np
 import json
@@ -14,7 +15,6 @@ from keras.layers import MaxPooling1D
 from keras.callbacks import EarlyStopping
 from sklearn.utils import shuffle
 from gensim.parsing.preprocessing import *
-
 
 CUSTOM_FILTERS = [  lambda x: x.lower(), #To lowercase
                     lambda text: re.sub(r'https?:\/\/.*\s', '', text, flags=re.MULTILINE), #To Strip away URLs
@@ -65,15 +65,12 @@ f.close()
 print('collected data successfully')
 
 
-max_size = 500
-num_categories = 3
-vocab_size = 30000 + 1
-embedding_size = 100
-nb_epochs = 10
+MAX_SIZE = 500
+NUM_CATEGORIES = 3
 
 print('preparing data..')
 x_train = pad_sequences(data, maxlen=max_size, truncating='post', padding='pre')
-y_train = prepare_labels(labels, num_categories)  
+y_train = prepare_labels(labels, NUM_CATEGORIES)  
 
 del data
 del labels
@@ -98,31 +95,78 @@ with open('test_data.csv', 'r', encoding="utf8") as csvfile:
         labels.append(r[-1])
                 
 x_test = pad_sequences(data[1:], maxlen=59, truncating='post', padding='pre')
-y_test = prepare_labels(labels[1:], num_categories)
-print('data prepared.')      
+y_test = prepare_labels(labels[1:], NUM_CATEGORIES)
+
+del data
+del labels
+del nums
+del d
+del words
+del reader
+
+print('data prepared.')     
+
+EMBEDDING_SIZES = [50, 100, 150, 200, 250, 300]
+N_RECURRENT_LAYERS = 3
+N_RECURRENT_UNITS = 100
+RECURRENT_DROPOUT = 0.3
+RECURRENT_LAYERS_TYPES = [GRU, LSTM]
+FILTER_SIZES = [64, 32, 16]
+VOCAB_SIZE = 30000 + 1
+NB_EPOCHS = 10
+
+base_dir = 'models {} epochs/'.format(NB_EPOCHS)
+
+if not os.path.exists(base_dir):
+    os.mkdir(base_dir)
+
+for emb_size in EMBEDDING_SIZES:
+    for recur_layer in RECURRENT_LAYERS_TYPES:
+        for i in range(1, N_RECURRENT_LAYERS+1):
+            model = Sequential()
+            model.add(Embedding(VOCAB_SIZE, emb_size, input_length=MAX_SIZE))
+            for j in range(i):
+                return_sequences = True
+                if j + 1 == i:
+                    return_sequences = False
+                model.add(Conv1D(filters=FILTER_SIZES[j], kernel_size=8, activation='relu'))
+                model.add(MaxPooling1D(pool_size=2))
+                model.add(recur_layer(N_RECURRENT_UNITS, recurrent_dropout=RECURRENT_DROPOUT, return_sequences=return_sequences))
+            model.add(Dense(250, activation='tanh'))
+            model.add(Dense(NUM_CATEGORIES, activation='softmax'))
+            #model.summary()
+            model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+            
+            es = EarlyStopping(monitor='val_loss', min_delta=0, patience=1, verbose=0, mode='auto')
+            history = model.fit(x_train, y_train, batch_size=128, shuffle=True, epochs=NB_EPOCHS, callbacks=[es], validation_split=0.2, verbose=1)
+
+            acc = history.history['acc']
+            val_acc = history.history['val_acc']
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+
+            test_loss, test_acc = model.evaluate(x_test,y_test)
+
+            model_info = 'Embedding size {0} - {1} Layers {2}'.format(emb_size, recur_layer.__name__, i)
+            model_dir = os.path.join(base_dir, model_info)
+
+            os.mkdir(model_dir)
+            os.chdir(model_dir)
+            headers = ['acc', 'loss', 'val_acc', 'val_loss', 'test_acc', 'test_loss']
+            results = [acc[-1], loss[-1], val_acc[-1], val_loss[-1], test_acc, test_loss]
+            with open('results.csv', 'w') as f:
+                w = csv.writer(f)
+                w.writerow(headers)
+                w.writerow(results)
+
+            model_json = model.to_json()
+            with open("model.json", "w") as json_file:
+                json_file.write(model_json)
+            model.save_weights("model.h5")
+            print("Saved model to disk")
+         
+            os.chdir('..')
+            os.chdir('..')
 
 
-
-
-model = Sequential()
-model.add(Embedding(vocab_size, embedding_size, input_length=max_size))
-model.add(Conv1D(filters=64, kernel_size=4, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(GRU(100, recurrent_dropout=0.3, return_sequences=True))
-model.add(Conv1D(filters=16, kernel_size=4, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(GRU(100, recurrent_dropout=0.3))
-model.add(Dense(250, activation='sigmoid'))
-model.add(Dense(3, activation='softmax'))
-print(model.summary())
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-
-es_cb = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
-
-model.fit(x_train, y_train, batch_size=128, shuffle=True, epochs=nb_epochs, callbacks=[es_cb], validation_split=0.2, verbose=1)
-
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-model.save_weights("model.h5")
-print("Saved model to disk")
+            
